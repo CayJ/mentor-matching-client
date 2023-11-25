6b7ea8b7791e10a68af54a98dbe29e37ff019eab
CayJ <35189356+CayJ@users.noreply.github.com>
2023-11-24 20:35:04 -0500

    D1 database access test

--- functions/api/profile/[[userID]].ts
+++ functions/api/profile/[[userID]].ts
@@ -1,5 +1,34 @@
-export const onRequest: PagesFunction = async (context) => {
+export interface Env {
+    // If you set another name in wrangler.toml as the value for 'binding',
+    // replace "DB" with the variable name you defined.
+    DB: D1Database;
+}
+export const onRequest: PagesFunction<Env> = async (context) => {
     const userID = context.params.userID;
 
-    return new Response(`User ID: ${userID}`);
+    // Check if userID is provided
+    if (!userID) {
+        return new Response("User ID not provided", { status: 400 });
+    }
+
+    try {
+        // Prepare the SQL query to fetch user data by ID
+        const query = "SELECT * FROM Users WHERE ID = ?"; // Adjust the table and column names as needed
+        const userData = await context.env.DB.prepare(query)
+            .bind(userID)
+            .first(); // Use 'get' for a single row, 'all' for multiple rows
+
+        // Check if user data is found
+        if (!userData) {
+            return new Response("User not found", { status: 404 });
+        }
+
+        // Return the user data
+        return new Response(JSON.stringify(userData), {
+            headers: { 'content-type': 'application/json' },
+        });
+    } catch (error) {
+        // Handle any errors during database access
+        return new Response(`Error accessing D1 Database: ${error}`, { status: 500 });
+    }
 }
